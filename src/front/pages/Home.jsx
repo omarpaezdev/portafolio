ad37678ed575b7013a59f32572857a0e07f93500
omarpaezdev <produc.tecnologico@gmail.com>
2025-12-16 19:24:35 +0100

    feat: implementacion inicial del portafolio

--- src/front/pages/Home.jsx
+++ src/front/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import Hero from "../components/Hero.jsx";
 
 export const Home = () => {
 
@@ -33,20 +34,24 @@ export const Home = () => {
 	}, [])
 
 	return (
-		<div className="text-center mt-5">
-			<h1 className="display-4">Hello Rigo!!</h1>
-			<p className="lead">
-				<img src={rigoImageUrl} className="img-fluid rounded-circle mb-3" alt="Rigo Baby" />
-			</p>
-			<div className="alert alert-info">
-				{store.message ? (
-					<span>{store.message}</span>
-				) : (
-					<span className="text-danger">
-						Loading message from the backend (make sure your python 🐍 backend is running)...
-					</span>
-				)}
-			</div>
+		<div className="text-center home-container">
+			<main>
+				<section id="hero">
+					<Hero/>
+				</section>
+				<section id="about">
+
+				</section>
+				<section id="skills">
+
+				</section>
+				<section id="projects">
+
+				</section>
+				<section id="contact">
+
+				</section>
+			</main>
 		</div>
 	);
 }; 
